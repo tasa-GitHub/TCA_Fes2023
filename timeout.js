ccba38d6d0906505b724f05a3196ad9e724940ec
polskaa <sen.0128.111@gmail.com>
2023-09-07 22:58:34 +0900

    first commit

---
+++ timeout.js
@@ -0,0 +1,33 @@
+let FLAG = false;
+let LASTTIME = Date.now();
+
+const WATCHEVENTS = [
+  'scroll',       // ウィンドウをスクロール
+  'resize',       // ウィンドウサイズを変更
+  'click',        // マウスの左ボタンをクリック
+  'contextmenu',  // マウスの右ボタンをクリック
+  'mousemove',    // マウスポインターを移動
+  'wheel',        // マウスのホイールを操作
+  'keypress',     // キーボードを押下
+  'touchstart',   // タッチ開始（スマホ）
+  'touchend',     // タッチ終了（スマホ）
+  'touchmove',    // タッチしながら移動（スマホ）
+  'touchcancel'   // タッチをキャンセル（スマホ）
+];
+
+window.addEventListener("load", () => {
+WATCHEVENTS.forEach((type) => {
+    window.addEventListener(type, () => {
+    LASTTIME = Date.now();
+    });
+});
+
+const timerId = setInterval(() => {
+    FLAG = Date.now() - LASTTIME > 60 * 1000;
+
+    if (FLAG) {
+    clearInterval(timerId);
+    window.location.href = "../index.html";
+    }
+}, 1000);
+});
